0f595a45d016be6045695062db3b62d78dd37e6e
Oli <oliverw92@mac.com>
2012-02-20 10:56:45 +0000

    Work on representing database entities in PHP for ease of use

---
+++ entities/entity.php
@@ -0,0 +1,22 @@
+<?php
+
+	class Entity {
+		
+		private $id;
+		private $name;
+		
+		public function __construct($id, $name) {
+			$this->id = $id;
+			$this->name = $name;
+		}
+		
+		public function getId() {
+			return $this->id;
+		}
+		public function getName() {
+			return $this->name;
+		}
+		
+	}
+	
+?>
