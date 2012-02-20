60b52535c66ccdb41e808c6cd9c881f70c76c82b
Oli <oliverw92@mac.com>
2012-02-20 11:10:08 +0000

    More work on entities

--- entities/room.php
+++ entities/room.php
@@ -8,6 +8,30 @@
 		private $park;
 		private $type;
 		
+		public function __construct($id, $name, $building, $park, $type) {
+			$this->id = $id;
+			$this->name = $name;
+			$this->building = $building;
+			$this->park = $park;
+			$this->type = $type;		
+		}
+		
+		public function getId() {
+			return $this->id;
+		}
+		public function getName() {
+			return $this->name;
+		}
+		public function getBuilding() {
+			return $this->building;
+		}
+		public function getPark() {
+			return $this->park;
+		}
+		public function getType() {
+			return $this->type;
+		}
+		
 	}
 
 ?>
--- entities/round.php
+++ entities/round.php
@@ -2,7 +2,35 @@
 
 	class Round {
 		
+		private $id;
+		private $round;
+		private $startDate;
+		private $endDate;
+		private $active;
 		
+		public function __construct($id, $round, $startDate, $endDate, $active) {
+			$this->id = $id;
+			$this->round = $round;
+			$this->startDate = $startDate;
+			$this->endDate = $endDate;
+			$this->active = $active;
+		}
+		
+		public function getId() {
+			return $this->id;
+		}
+		public function getRound() {
+			return $this->round;
+		}
+		public function getStartDate() {
+			return $this->startDate;
+		}
+		public function getEndDate() {
+			return $this->endDate;
+		}
+		public function getActive() {
+			return $this->active;
+		}
 		
 	}
 
